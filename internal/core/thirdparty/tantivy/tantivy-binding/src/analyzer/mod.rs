53cd216884c1e9f2ec4d3e963d7ea4e1e1427e37
gf53520 <guifengleaf@gmail.com>
2026-05-08 20:11:50 +0800

    clear code

--- internal/core/thirdparty/tantivy/tantivy-binding/src/analyzer/mod.rs
+++
@@ -1,9 +0,0 @@
-mod analyzer;
-mod build_in_analyzer;
-mod dict;
-mod filter;
-
-pub mod tokenizers;
-pub use self::analyzer::{create_analyzer, create_analyzer_by_json};
-
-pub(crate) use self::build_in_analyzer::standard_analyzer;
